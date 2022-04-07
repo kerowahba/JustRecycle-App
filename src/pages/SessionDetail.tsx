7067fb855e2a581235404b30a9e187a34b35abc7
Kerolos Wahba <keroedwar@ymail.com>
2022-04-06 22:28:55 -0400

    Revert "Scan History is done"

--- src/pages/SessionDetail.tsx
+++ src/pages/SessionDetail.tsx
@@ -46,12 +46,6 @@ const SessionDetail: React.FC<SessionDetailProps> = ({ session, addFavorite, rem
             <IonBackButton defaultHref="/tabs/schedule"></IonBackButton>
           </IonButtons>
           <IonButtons slot="end">
-            <IonButton onClick={() => toggleFavorite()}>
-              {isFavorite ?
-                <IonIcon slot="icon-only" icon={star}></IonIcon> :
-                <IonIcon slot="icon-only" icon={starOutline}></IonIcon>
-              }
-            </IonButton>
             <IonButton onClick={() => shareSession}>
               <IonIcon slot="icon-only" icon={share}></IonIcon>
             </IonButton>
@@ -72,21 +66,8 @@ const SessionDetail: React.FC<SessionDetailProps> = ({ session, addFavorite, rem
           </IonText>
         </div>
         <IonList>
-          <IonItem onClick={() => sessionClick('watch')} button>
-            <IonLabel color="primary">Watch</IonLabel>
-          </IonItem>
           <IonItem onClick={() => sessionClick('add to calendar')} button>
             <IonLabel color="primary">Add to Calendar</IonLabel>
-          </IonItem>
-          <IonItem onClick={() => sessionClick('mark as unwatched')} button>
-            <IonLabel color="primary">Mark as Unwatched</IonLabel>
-          </IonItem>
-          <IonItem onClick={() => sessionClick('download video')} button>
-            <IonLabel color="primary">Download Video</IonLabel>
-            <IonIcon slot="end" color="primary" size="small" icon={cloudDownload}></IonIcon>
-          </IonItem>
-          <IonItem onClick={() => sessionClick('leave feedback')} button>
-            <IonLabel color="primary">Leave Feedback</IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
